628c8426c632381861f07cdf08160914094a2058
charlielobster <erik.brobyn@gmail.com>
2025-09-14 05:32:31 -0400

    ping

--- home/USER/CUDA_NekRS_vars.sh
+++ home/USER/CUDA_NekRS_vars.sh
@@ -4,8 +4,12 @@ export CUDA_LIB=$CUDA_HOME/lib64
 export UCX_HOME=/opt/ucx-1.20.0
 export UCX_LIB=$UCX_HOME/lib
 
+export UCX_TLS="cuda"
+export UCX_NET_DEVICES="wlp5s0"
+
 export OMPI_HOME=/opt/ompi-5.0.08
 export OMPI_LIB=$OMPI_HOME/lib
 
 export PATH=$CUDA_HOME/bin:$UCX_HOME/bin:$OMPI_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_LIB:$UCX_LIB:$OMPI_LIB:$LD_LIBRARY_PATH
+
