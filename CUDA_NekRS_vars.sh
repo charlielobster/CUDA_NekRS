ce2bb9165090c49c77af8112556cc0565c31b261
charlielobster <erik.brobyn@gmail.com>
2025-09-18 23:26:32 -0400

    edits

--- CUDA_NekRS_vars.sh
+++ CUDA_NekRS_vars.sh
@@ -9,14 +9,14 @@ export GDRCOPY_HOME=~/builds/gdrcopy
 export PATH=$GDRCOPY_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$GDRCOPY_HOME/lib:$LD_LIBRARY_PATH
 
-export UCX_HOME=~/builds/ucx-1.20.0
+export UCX_HOME=~/builds/UCX-1.20.0
 export UCX_LIB=$UCX_HOME/lib
 
 export UCX_TLS="cuda"
 export UCX_NET_DEVICES="wlp5s0" # your nic here
 
 export PATH=$UCX_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$UCX_HOME:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$UCX_HOME/lib:$LD_LIBRARY_PATH
 
 # export OMPI_HOME=~/builds/openmpi-5.0.8 # can go anywhere
 # export OMPI_LIB=$OMPI_HOME/lib
