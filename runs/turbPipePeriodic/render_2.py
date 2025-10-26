2cef0c70ec36ab6be84ef442f0f530b413278237
charlielobster <erik.brobyn@gmail.com>
2025-10-26 18:03:07 -0400

    turbPipePeriodic files

---
+++ runs/turbPipePeriodic/render_2.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from paraview.simple import *
+
+LoadState('/media/skooby/data/NekRS_runs/turbPipePeriodic/turbPipe.pvsm')
+renderView = GetRenderView()
+renderView.ViewSize = [3840, 2160]
+SaveAnimation('turbPipe.png', renderView, FrameWindow=[4167, 8335], ImageResolution=renderView.ViewSize, CompressionLevel=0)
