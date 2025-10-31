85681296c4bd267b16a2426d77ed1ac66851ea27
charlielobster <erik.brobyn@gmail.com>
2025-10-31 16:09:38 -0400

    comments in reindex script

--- bin/reindex_nek5000_extensions.sh
+++ bin/reindex_nek5000_extensions.sh
@@ -6,29 +6,24 @@ fi
 
 cd $1 # go to the target directory
 
-regex="^f0[0-9]+$" # search for a string with characters "f0nnnnn", where n is in [0-9]
+# use the maximum extension length matching nekRS file extension convention (.f0nnnn)
+len=$(ls -t *.f0* | awk -F. '{print $NF}' | awk '{print length}' | sort -nr | head -n 1)
+len=$((len - 1)) # remove leading character 'f'
+printf_string="%0${len}d" # printf format string with leading zeros 
 
 offset=$2 # an integer to add to each file's extension
 
-for i in $(ls -t); # for each file in a call to ls
+for i in $(ls -t *.f0*); # for each file in a call to ls
 do
-    IFS="." read -ra string_array <<< "$i" # split the file on '.'
+    IFS="." read -ra string_array <<< "$i" # split the file's name on '.'
+    ext=${string_array[1]} # file extension
+    oldnum=${ext:1} # strip the leading 'f' from the extension string
+    oldnum=$((10#$oldnum)) # strip leading zeros so arithmetic avoids base 8 calculation
 
-    if [[ ${string_array[1]} =~ $regex ]]; then # if the file's extension matches our target...
+    newnum=$((oldnum + offset)) # perform addition
 
-        # len should be the same for all files, so it would be better to do this check before this for loop
-        len=$((${#string_array[1]} - 1)) # get the length of the extension from ls 
-
-        printf_string="%0${len}d" # better up front with where len should be defined
-
-        oldnum=${string_array[1]:1} # string the leading 'f' from the extension string
-        oldnum=$((10#$oldnum)) # strip leading zeros so arithmetic avoids base 8 calculation
-
-        newnum=$((oldnum + offset)) # perform addition
-
-        printf -v newext $printf_string $newnum # cast it formatted string with leading zeros and new offset
-        
-        mv $i ${string_array[0]}'.f'$newext # rename the file
-    fi
+    printf -v newext $printf_string $newnum # cast it formatted string with leading zeros and new offset
+    
+    mv $i ${string_array[0]}'.f'$newext # rename the file
 done
 
