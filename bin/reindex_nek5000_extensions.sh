e8d678fbb7bf1c20b1fbde74d19a9a2838f7dc1f
charlielobster <erik.brobyn@gmail.com>
2025-10-31 15:18:12 -0400

    comments on bash

--- bin/reindex_nek5000_extensions.sh
+++ bin/reindex_nek5000_extensions.sh
@@ -4,29 +4,31 @@ if [ $# -ne 2 ]; then
     echo "reindex_nek5000_extensions <path> <new offset>"
 fi
 
-cd $1
+cd $1 # go to the target directory
 
-regex="^f0[0-9]+$"
+regex="^f0[0-9]+$" # search for a string with characters "f0nnnnn", where n is in [0-9]
 
-offset=$2
+offset=$2 # an integer to add to each file's extension
 
-for i in $(ls -t);
+for i in $(ls -t); # for each file in a call to ls
 do
-    IFS="." read -ra string_array <<< "$i"
+    IFS="." read -ra string_array <<< "$i" # split the file on '.'
 
-    if [[ ${string_array[1]} =~ $regex ]]; then
+    if [[ ${string_array[1]} =~ $regex ]]; then # if the file's extension matches our target...
 
-        len=$((${#string_array[1]} - 1))
-        printf_string="%0${len}d\n"
+        # len should be the same for all files, so it would be better to do this check before this for loop
+        len=$((${#string_array[1]} - 1)) # get the length of the extension from ls 
 
-        oldnum=${string_array[1]:1}
-        oldnum=$((10#$oldnum)) #strip leading zeros for arithmetic to avoid base 8 calculation
+        printf_string="%0${len}d" # better up front with where len should be defined
 
-        newnum=$((oldnum + offset))
+        oldnum=${string_array[1]:1} # string the leading 'f' from the extension string
+        oldnum=$((10#$oldnum)) # strip leading zeros so arithmetic avoids base 8 calculation
 
-        printf -v newext $printf_string $newnum
+        newnum=$((oldnum + offset)) # perform addition
 
-        mv ${string_array[0]}.${string_array[1]} ${string_array[0]}'.f'$newext
+        printf -v newext $printf_string $newnum # cast it formatted string with leading zeros and new offset
+        
+        mv $i ${string_array[0]}'.f'$newext # rename the file
     fi
 done
 
