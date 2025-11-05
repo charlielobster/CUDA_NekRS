d7ec591d07bd9b61a5b9ac84ce8172db9712c277
charlielobster <erik.brobyn@gmail.com>
2025-11-05 11:52:19 -0500

    comments

--- bin/create_mp4.sh
+++ bin/create_mp4.sh
@@ -3,6 +3,8 @@
 
 if [ $# -ne 2 ]; then
     echo "create_mp4 <png regex path> <path to mp4>"
+    # for example, 
+    # ./create_mp4.sh .../CUDA_NekRS/artifacts/pngs/tcf.%04d.png .../CUDA_NekRS/artifacts/mp4s/tcf.mp4
 fi
 
 ffmpeg -framerate 50 -i $1 -c:v libx264 -pix_fmt yuv420p $2
